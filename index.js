0fd5ab6b3d7334d202254bd30b8050dc9db47253
たけのこII <113168697+Takenoko-II@users.noreply.github.com>
2024-07-01 17:28:13 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("foo").addEventListener("click", () => {
         alert("ソースコードに移動しますん");
-        location.href = "https://github.com/Takenoko-II/Takenoko-II.github.com";
+        location.href = "https://github.com/Takenoko-II/Takenoko-II.github.io";
     });
 });
